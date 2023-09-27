547e5534694d2706e2c7cf5a04784fa97a236157
EmanuelALemos <araujoemanuel97@gmail.com>
2023-09-27 09:02:21 -0300

    Criação do projeto .java e da primeira tela Novo jogo

---
+++ LibraDesk1/src/main/java/com/mycompany/libradesk1/LibraDesk1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.libradesk1;
+
+/**
+ *
+ * @author arauj
+ */
+public class LibraDesk1 {
+
+    public static void main(String[] args) {
+       new New_Book().setVisible(true);
+    }
+}
