d9a464661a1130182fc3d653f8c08816212135f4
EmanuelALemos <araujoemanuel97@gmail.com>
2023-10-03 12:24:52 -0300

    Divisão das pastas do projeto e implentação da navegação entre as telas de acervo e novo livro

--- LibraDesk/src/controller/LibraDeskNovoLivroController.java
+++ LibraDesk/src/controller/LibraDeskNovoLivroController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package libradesk;
+package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,9 +11,9 @@ import javafx.fxml.Initializable;
 /**
  * FXML Controller class
  *
- * @author CAIO
+ * @author arauj
  */
-public class LibraDeskFXController implements Initializable {
+public class LibraDeskNovoLivroController implements Initializable {
 
     /**
      * Initializes the controller class.
---
+++ LibraDesk/src/controller/Main.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controller; 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    private static Stage stage;
+    
+    private static Scene Acervo;
+    private static Scene NovoLivro;
+    
+    @Override
+    public void start(Stage primaryStage)throws Exception{
+        primaryStage.setTitle("LibraDesk");
+        
+        Parent xmlAcervo = FXMLLoader.load(getClass().getResource("../view/Acervo.fxml"));
+        Acervo = new Scene(xmlAcervo, 1280,720);
+        
+        Parent fxmlNovoLivro = FXMLLoader.load(getClass().getResource("../view/NovoLivro.fxml"));
+        NovoLivro = new Scene(fxmlNovoLivro, 992,614);
+        
+        primaryStage.setScene(Acervo);
+        primaryStage.show();
+    }
+    
+    public static void changeScreen(String src){
+        switch (src){
+            case "acervo":
+                stage.setScene(Acervo);
+                break;
+            case "novoLivro":
+                stage.setScene(NovoLivro);
+                break;
+        }
+    }
+    public static void main(String[] args ) {
+        launch(args);
+    }
+    
+}
